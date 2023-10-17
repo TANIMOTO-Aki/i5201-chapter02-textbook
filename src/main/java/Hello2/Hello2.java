356302e270dfc7244bcb05f34788a0fcd5a5396d
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-10-17 15:55:46 +0900

    Hello2.javaを作成しました

---
+++ src/main/java/Hello2/Hello2.java
@@ -0,0 +1,10 @@
+/*
+ * Hello!
+ * How you doing?　と表示するプログラム
+ */
+public class Hello2 {
+   public static void main(String[] args) {
+    System.out.println("Hello!");           // １行目の表示
+    System.out.println("How you doing?");   // ２行目の表示
+   } 
+}
